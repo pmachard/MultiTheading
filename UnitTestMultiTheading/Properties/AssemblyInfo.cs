03a6c3d22163ddecdb8c95d8b0e81d61a9a80177
pascal machard <pascal.machard@gmail.com>
2018-03-29 18:43:16 +0200

    Sauvegarde des fichiers

---
+++ UnitTestMultiTheading/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestMultiTheading")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestMultiTheading")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("624aae5c-a4ca-48e0-9865-8c01a9f86424")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestMultiTheading/UnitTestMultiTheading.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading;
+using System.Linq;
+
+namespace UnitTestMultiTheading
+{
+    [TestClass]
+    public class UnitTestMultiTheading
+    {
+        private Thread myThread;
+        static int cpt = 0;
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            myThread = new Thread(new ThreadStart(ThreadLoop));
+            myThread.Start();
+            while (cpt < 10)
+            {
+
+            }
+        }
+
+        public static void ThreadLoop()
+        {
+            while (Thread.CurrentThread.IsAlive)
+            {
+                Thread.Sleep(500);
+                cpt++;
+                Console.WriteLine("Je travaille...");
+            }
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            var result = string.Join(" ", array);
+        }
+
+    }
+
+    interface IA
+    {
+        void fct();
+    }
+
+    class A : IA
+    {
+        public void fct() { }
+    }
+
+    class B : A
+    {
+    }
+
+    class C : A
+    {
+    }
+}
